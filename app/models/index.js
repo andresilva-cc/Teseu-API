9c732f5e8cde0409b5d40ff9a8027c4a44593327
André Luiz da Silva <andreluiz.97@outlook.com>
2019-06-23 17:33:56 -0300

    Atualiza estrutura do projeto

--- app/models/index.js
+++ app/models/index.js
@@ -1,40 +1,37 @@
-// Imports
+'use strict';
+
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
+const env = process.env.ENVIRONMENT || 'development';
+const config = require(__dirname + '/../config/sequelize.js')[env];
+const db = {};
 
-const basename = path.basename(module.filename);
-const db = {}
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
-// Instantiate Sequelize with the provided configuration
-const sequelize = new Sequelize(
-  process.env.DB_HOST,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'postgres'
-  }
-);
-
-// Import Models
 fs
   .readdirSync(__dirname)
-  .filter(file =>
-    (file.indexOf('.') !== 0) &&
-    (file !== basename) &&
-    (file.slice(-3) === '.js'))
-  .forEach(file => {
-    const model = sequelize.import(path.join(__dirname, file))
-    db[model.name] = model
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
+  .forEach(file => {
+    const model = sequelize['import'](path.join(__dirname, file));
+    db[model.name] = model;
+  });
 
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
-    db[modelName].associate(db)
+    db[modelName].associate(db);
   }
-})
+});
 
-db.sequelize = sequelize
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
-// Export Models and Sequelize instance
-module.exports = db
+module.exports = db;
