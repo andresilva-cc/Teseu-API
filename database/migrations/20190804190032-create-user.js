1f8ac80be332d0229a7cc3e125ddebc68781b78c
André Luiz da Silva <andreluiz.97@outlook.com>
2019-08-04 16:42:39 -0300

    Altera estilo do código

--- database/migrations/20190804190032-create-user.js
+++ database/migrations/20190804190032-create-user.js
@@ -1,4 +1,5 @@
-'use strict';
+'use strict'
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('Users', {
@@ -36,9 +37,10 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    })
   },
+
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Users');
+    return queryInterface.dropTable('Users')
   }
-};
+}
