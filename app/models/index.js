10993f018cfcac920fbf8d43f684708ab2766aec
André Luiz da Silva <andreluiz.97@outlook.com>
2019-08-05 22:50:25 -0300

    Refatora index dos Models

--- app/models/index.js
+++ app/models/index.js
@@ -1,4 +1,4 @@
-const Sequelize = require("sequelize")
+const Sequelize = require('sequelize')
 
 const env = process.env.ENVIRONMENT || 'development'
 const config = require(__dirname + '/../../config/sequelize.js')[env]
@@ -6,7 +6,7 @@ const config = require(__dirname + '/../../config/sequelize.js')[env]
 const sequelize = new Sequelize(config.database, config.username, config.password, config)
 
 // Models
-const CategoryModel = require("./category")
+const CategoryModel = require('./category')
 
 // Init Models
 const models = {
@@ -15,7 +15,7 @@ const models = {
 
 // Run associations if it exists
 Object.values(models)
-  .filter(model => typeof model.associate === "function")
+  .filter(model => typeof model.associate === 'function')
   .forEach(model => model.associate(models))
 
 const db = {
