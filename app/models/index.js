f41fc90a1d75def2b09a6dd7111674ab06008258
André Luiz da Silva <andreluiz.97@outlook.com>
2019-08-05 22:48:43 -0300

    Refatora index dos Models

--- app/models/index.js
+++ app/models/index.js
@@ -1,37 +1,26 @@
-'use strict';
+const Sequelize = require("sequelize")
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
-const env = process.env.ENVIRONMENT || 'development';
-const config = require(__dirname + '/../config/sequelize.js')[env];
-const db = {};
+const env = process.env.ENVIRONMENT || 'development'
+const config = require(__dirname + '/../../config/sequelize.js')[env]
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+const sequelize = new Sequelize(config.database, config.username, config.password, config)
+
+// Models
+const CategoryModel = require("./category")
+
+// Init Models
+const models = {
+  Category: CategoryModel.init(sequelize, Sequelize)
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
-  });
+// Run associations if it exists
+Object.values(models)
+  .filter(model => typeof model.associate === "function")
+  .forEach(model => model.associate(models))
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+const db = {
+  ...models,
+  sequelize
+}
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
+module.exports = db
