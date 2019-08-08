846be43b19e4f14fb554a09fca4d94c12c8f6c63
André Luiz da Silva <andreluiz.97@outlook.com>
2019-08-08 20:13:28 -0300

    Adiciona operações básicas que faltavam no Base Repository

--- app/repositories/base_repository.js
+++ app/repositories/base_repository.js
@@ -16,6 +16,21 @@ class BaseRepository {
   }
 
   /**
+   * Retrieves a resource by its ID
+   *
+   * @param {number} id ID of the resource
+   * @returns {Object|null} The resource if found, null if not
+   * @memberof BaseRepository
+   */
+  async findById (id) {
+    try {
+      return await this.model.findByPk(id)
+    } catch (ex) {
+      throw ex
+    }
+  }
+
+  /**
    * Creates a new resource
    *
    * @param {Object} data Data of the new resource
@@ -25,6 +40,39 @@ class BaseRepository {
   async create (data) {
     try {
       return await this.model.create(data)
+    } catch (ex) {
+      throw ex
+    }
+  }
+
+  /**
+   * Updates a resource
+   *
+   * @param {number} id ID of the resource
+   * @param {Object} data Data to update
+   * @returns {Object} The updated resource
+   * @memberof BaseRepository
+   */
+  async update (id, data) {
+    try {
+      const resource = await this.findById(id)
+      return await resource.update(data)
+    } catch (ex) {
+      throw ex
+    }
+  }
+
+  /**
+   * Deletes a resource
+   *
+   * @param {number} id ID of the resource
+   * @returns {number} Number of rows deleted
+   * @memberof BaseRepository
+   */
+  async delete (id) {
+    try {
+      const resource = await this.findById(id)
+      return await resource.destroy()
     } catch (ex) {
       throw ex
     }
