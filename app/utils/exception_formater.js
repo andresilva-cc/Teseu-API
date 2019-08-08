24ef9eb65c3535278964ec4790efe5ffb491ce74
André Luiz da Silva <andreluiz.97@outlook.com>
2019-08-07 21:21:02 -0300

    Adiciona classe ExceptionFormatter para padronizar retorno de erros

---
+++ app/utils/exception_formater.js
@@ -0,0 +1,66 @@
+const Helpers = require('./helpers')
+
+/** Class to format exceptions to a standard format */
+class ExceptionFormatter {
+  
+  /**
+   * Formats an exception
+   *
+   * @static
+   * @param {Object} ex - The exception to be formatted
+   * @returns {Object} The formatted exception
+   * @memberof ExceptionFormatter
+   */
+  static format (ex) {
+    // Parse exception
+    const exception = this.parse(ex)
+
+    // If in development, add stack
+    if (process.env.ENVIRONMENT === 'development') {
+      exception.error.stack = ex.stack
+    }
+    
+    return exception
+  }
+  
+  /**
+   * Parses the exception name and builds the new error object
+   *
+   * @static
+   * @param {*} ex - The exception to be parsed
+   * @returns {Object} The formatted exception
+   * @memberof ExceptionFormatter
+   */
+  static parse (ex) {
+    switch (ex.name) {
+      case 'SequelizeConnectionRefusedError':
+        return {
+          code: 503,
+          error: {
+            message: 'Service Unavailable',
+            details: 'The service is unavailable due to a database connection error. Try again later.'
+          }
+        }
+
+      case 'SequelizeUniqueConstraintError':
+        return {
+          code: 400,
+          error: {
+            message: 'Validation Error',
+            details: Helpers.capitalizeFirstLetter(ex.errors[0].message)
+          }
+        }
+
+      default:
+        return {
+          code: 500,
+          error: {
+            message: 'Unknown Error',
+            details: 'An error occurred, but it could not be identified.'
+          }
+        }
+    }
+  }
+}
+
+module.exports = ExceptionFormatter
