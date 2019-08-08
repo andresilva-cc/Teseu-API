52130e87e5c077097e948a4f5369710fe50bbc8d
André Luiz da Silva <andreluiz.97@outlook.com>
2019-08-07 22:03:51 -0300

    Adiciona import automático de rotas

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const express = require('express');
 // Create Express instance
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Welcome to Teseu API, check https://web.teseu.app/api-docs for more information on how to use this API');
+})
+
 // Import routes
-const routes = require('./routes');
-app.use('/', routes);
+require('./routes')(app);
 
 // Start server
 app.listen(process.env.APP_PORT, () => {
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,10 @@
-// Imports
-const express = require('express');
-const router = express.Router();
+const fs = require('fs')
 
-// Routes
-router.get('/', (req, res) => {
-  res.send('Welcome to Teseu API, check https://web.teseu.app/api-docs for more information on how to use this API');
-});
-
-// Export Router
-module.exports = router;
+module.exports = function (app) {
+  fs.readdirSync(__dirname).forEach(function(file) {
+    if (file === "index.js" || file.substr(file.lastIndexOf('.') + 1) !== 'js')
+      return
+    const name = file.substr(0, file.indexOf('.'))
+    require('./' + name)(app)
+  })
+}
