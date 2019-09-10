b12b8ca5c33fb6a38befed0fcead0764c69ba39b
André Luiz da Silva <andreluiz.97@outlook.com>
2019-09-09 21:54:53 -0300

    Adiciona migration para adicionar userId a Occurrences

---
+++ database/migrations/20190910004051-add-user-id-to-occurrences.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('Occurrences', 'userId', {
+      allowNull: false,
+      type: Sequelize.INTEGER,
+      onDelete: 'CASCADE',
+      references: {
+        model: 'Users',
+        key: 'id',
+        as: 'userId'
+      }
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('Occurrences', 'userId')
+  }
+}
