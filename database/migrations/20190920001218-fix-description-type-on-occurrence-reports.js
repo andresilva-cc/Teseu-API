5a8931da026e0ec0136f4a51424e58b10d368f40
André Luiz da Silva <andreluiz.97@outlook.com>
2019-09-19 21:18:08 -0300

    Adiciona migration para corrigir tipo da coluna Description de OccurrenceReport

---
+++ database/migrations/20190920001218-fix-description-type-on-occurrence-reports.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return Promise.all([
+      queryInterface.removeColumn('OccurrenceReports', 'description'),
+      queryInterface.addColumn('OccurrenceReports', 'description', {
+        allowNull: false,
+        type: Sequelize.TEXT,
+      })
+    ])
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return Promise.all([
+      queryInterface.removeColumn('OccurrenceReports', 'description'),
+      queryInterface.addColumn('OccurrenceReports', 'description', {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      })
+    ])
+  }
+}
