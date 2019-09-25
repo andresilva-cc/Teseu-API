f3b765db558822e3e01b44dc78ff64e01acd8261
André Luiz da Silva <andreluiz.97@outlook.com>
2019-09-24 23:53:16 -0300

    Adiciona opção de executar servidor em HTTPS

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 require('dotenv').config()
+const https = require('https')
+const fs = require('fs')
 const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
@@ -25,7 +27,20 @@ require('./routes')(app)
 // Handle errors
 app.use(ErrorFormatter.middleware.bind(ErrorFormatter))
 
-// Start server
-app.listen(process.env.APP_PORT, () => {
-  console.log(`Listening on port ${process.env.APP_PORT}`)
-})
+// Create HTTPS server if set in env
+if (process.env.APP_USE_HTTPS) {
+  const options = {
+    key: fs.readFileSync(process.env.APP_HTTPS_KEY),
+    cert: fs.readFileSync(process.env.APP_HTTPS_CERT)
+  }
+  
+  https.createServer(options, app).listen(APP_HTTPS_PORT, () => {
+    console.log(`HTTPS: Listening on port ${process.env.APP_HTTPS_PORT}`)
+  })
+
+// Else, create HTTP server
+} else {
+  app.listen(process.env.APP_HTTP_PORT, () => {
+    console.log(`HTTP: Listening on port ${process.env.APP_HTTP_PORT}`)
+  })
+}
